d36ce3f68c7c6de011a2a6a4dd987986886adb31
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-04-04 20:38:14 -0500

    Update db credentials

--- app/constants/database.js
+++ app/constants/database.js
@@ -1,7 +1,7 @@
 const database = {
     HOST: 'localhost',
-    USER: 'root', // Change credentials
-    PASSWORD: 'root', // Change credentials
+    USER: 'collaborator', // Change credentials
+    PASSWORD: 'Root#123', // Change credentials
     DATABASE: "collaborator_db",
     TIMEOUT: 20000
 }
