37124ebe7e3c0ebc8aed2a680ec63c56efbfabc7
Khanatoshs <leo.hs334@gmail.com>
2021-05-02 16:20:47 -0500

    Cambios en scripts de instalacion

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/projects/TP2_test
-
-#navigate into our working directory where we have all our github files
-cd /home/ubuntu/projects/TP2_test
-
-#install node modules
-npm install
-
-#start our node app in the background
-pm2 restart collab-be > app.out.log 2> app.err.log < /dev/null & 
+set -e
+# Stop all servers and start the server as a daemon
+echo "STARTING APP"
+sudo forever stopall
+sudo forever start /home/ubuntu/projects/collab-be/index.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,3 @@
-
-
-#create our working directory if it doesnt exist
-DIR="/home/ubuntu/projects/TP2_test"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+#Installing forever before any installation
+echo "INSTALLING FOREVER"
+sudo npm install forever -g
